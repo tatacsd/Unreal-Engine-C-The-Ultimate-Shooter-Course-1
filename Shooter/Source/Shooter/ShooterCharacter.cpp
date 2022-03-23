e86bec213a7790a9d39ec5a7ac071e47fa71f765
Thays Casado <thayscasado@hotmail.com>
2022-03-23 00:40:08 -0700

    10 UE_LOG formatted specifiers -> for int, double, float and boolean

--- Shooter/Source/Shooter/ShooterCharacter.cpp
+++ Shooter/Source/Shooter/ShooterCharacter.cpp
@@ -22,6 +22,26 @@ void AShooterCharacter::BeginPlay()
 	//using brace initialization we are garanteed we will initialize (without entering a value it will be zero
 	int myInt{ 42 };
 	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
+
+
+	float myFloat{ 3.12159f }; // without f it would be a double and give an error
+	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
+
+	double myDouble{ 0.000756 };
+	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
+
+	char myChar{ 'J' };
+	UE_LOG(LogTemp, Warning, TEXT("char myChar: %c"), myChar);
+
+	wchar_t wideChar{ L'J' }; // the L gives the WILD CHARACTER
+	UE_LOG(LogTemp, Warning, TEXT("wchar_t wideChar: %lc"), wideChar);
+
+	bool myBool{ true };
+	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool); // d because it is converted to int
+
+	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f, bool: %d"), myInt, myFloat, myBool);
+
+
 }
 
 // Called every frame
