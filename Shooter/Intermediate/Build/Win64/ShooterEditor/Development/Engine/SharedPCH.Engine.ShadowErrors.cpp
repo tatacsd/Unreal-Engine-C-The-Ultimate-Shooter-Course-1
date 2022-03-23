848aeea04cb25b7b79937285ac4d53079b171bb3
Thays Casado <thayscasado@hotmail.com>
2022-03-23 00:07:30 -0700

    07. project setup - create blueprint and added as game default and added map as default

---
+++ Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30141
+#include "D:/UNREAL/Unreal-Engine-C-The-Ultimate-Shooter-Course/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
