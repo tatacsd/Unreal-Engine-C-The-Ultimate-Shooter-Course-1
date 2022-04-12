00f07aabe7ac952be241e0ef8d1940dd4ac02c45
Thays <thayscasado@hotmail.com>
2022-04-12 12:49:37 -0700

    git ignore~

--- README.md
+++
@@ -1,2 +0,0 @@
-# Unreal Engine C++ The Ultimate Shooter Course
-https://www.udemy.com/course/unreal-engine-the-ultimate-shooter-course/learn/lecture/26765178#overview
--- Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.31.31105
-#include "C:/HDD/UnrealProjects/Unreal-Engine-C-The-Ultimate-Shooter-Course/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
